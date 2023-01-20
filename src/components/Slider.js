5708db1b4bb8cde342429ba2678f8a33cd12382f
Matias Schmidt <matias.schmidt@outlook.es>
2023-01-19 21:25:37 -0300

    Agregando funcionalidad al componente slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,33 +1,63 @@
 import React, { useState } from "react";
 import logo from "../assets/ProFlight.png";
+import "./CSS/slider.css"
 
 
 
 function Slider() {
-  const data = [
-    {id: 1, image: logo},
-    {id: 2, image: logo},
-    {id: 3, image: logo},
-    {id: 4, image: logo},
-  ]
+    const data = [
+        {id: 1, image: logo},
+        {id: 2, image: logo},
+        {id: 3, image: logo},
+        {id: 4, image: logo},
+    ]
 
-  const [selectedItemId, setSelectedItemId] = useState(1)
+    const [selectedItemId, setSelectedItemId] = useState(1)
 
-  return (
-    <div className="container-slider">
-      <div className="container-items">
-        {data.map((item) => (
-            <div 
-              key={item.id} 
-              className={`circle ${selectedItemId > item.id ? 'active' : selectedItemId == item.id && 'current'}`} 
-            >
-              <img src={item.image} alt="Logo"/>
+
+    const handleNext = () => {
+        let newSelectedItemId
+        if (selectedItemId > data.length-1) {
+          newSelectedItemId = 1
+        } else {
+          newSelectedItemId = selectedItemId + 1
+        }
+        setSelectedItemId(newSelectedItemId)
+    };
+    
+    const handlePrev = () => {
+        let newSelectedItemId
+        if (selectedItemId < 2) {
+          newSelectedItemId = 4
+        } else {
+          newSelectedItemId = selectedItemId - 1
+        }
+        setSelectedItemId(newSelectedItemId)
+    };
+    
+    const handleItemClick = (item) => {
+        setSelectedItemId(item.id)
+    }
+
+    return (
+        <div className="container-slider">
+            <button onClick={handlePrev} className="btn" id='prev'> Prev </button>
+
+            <div className="container-items">
+                {data.map((item) => (
+                    <div 
+                    key={item.id} 
+                    className={`item ${selectedItemId == item.id ? 'active' : ""}`}
+                    onClick={() => handleItemClick(item)}
+                    >
+                    <img src={item.image} alt="Logo"/>
+                    </div>
+                ))}
             </div>
-        ))}
-      </div>
-
-    </div>
-  );
+            
+            <button onClick={handleNext} className="btn" id='next'> Next </button>
+        </div>
+    );
 }
 
 export default Slider;
